969ccc37f3b249fd375c765c611cbf71d554272f
Arkkarachat  Siribout <arkkarachat.s@gmail.com>
2020-09-15 20:47:26 +0700

    Moneymoney: Login Service

--- moneymoney/src/app/services/login.service.ts
+++ moneymoney/src/app/services/login.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {environment} from '../../environments/environment';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 
 
@@ -18,15 +19,17 @@ export class LoginService {
     return loginResult != null;
   }
 
-  login(values){
+  login(values): Observable<any>{
     return this.http.post<any>(`${this.hostUrl}/login`, values);
   }
 
-  logout(){
-    const headers = new HttpHeaders({
-      Accept: 'application/json',
-      Authorization:  `Bearer ${localStorage.getItem(environment.token)}`
+  logout(): Observable<any>{
+    const httpHeaders = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'X-Requested-With': 'XMLHttpRequest',
+      Authorization: `Bearer ${localStorage.getItem(environment.token)}`
     });
-    return this.http.post<any>(`${this.hostUrl}/logout`, null, {headers});
+
+    return this.http.post<any>(`${this.hostUrl}/logout`, null, {headers: httpHeaders});
   }
 }
